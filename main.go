cf842491a036be910ea8852c62520689b9d9bbe7
Ashermuzic <ashersam116@gmail.com>
2024-04-30 13:41:18 -0900

    initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Profile struct {
+	TotalSolved int `json:"totalSolved"`
+}
+
+func getNumberOfDays(startTime int64) int {
+	curTime := time.Now().Unix()
+
+	differenceInSeconds := curTime - startTime
+	differenceInDays := differenceInSeconds / (60 * 60 * 24)
+
+	return int(differenceInDays)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	leetCodeHandler := os.Getenv("LEETCODEHANDLER")
+
+	// try not to panic for everything
+	resp, err := http.Get("https://leetcode-stats-api.herokuapp.com/" + leetCodeHandler)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var profile Profile
+	if err := json.Unmarshal(body, &profile); err != nil {
+		panic(err)
+	}
+
+	// stated with 200 questions solved at the start date
+	startQuestions := 200
+	curSolved := profile.TotalSolved
+	startDate := time.Date(2024, time.April, 15, 15, 30, 0, 0, time.UTC).Unix()
+
+	expectedToSolve := (getNumberOfDays(startDate) * 3) + startQuestions
+
+	if expectedToSolve > curSolved {
+		fmt.Printf("You are behind by %d questions!!", expectedToSolve-curSolved)
+	} else if expectedToSolve < curSolved {
+		fmt.Printf("Keep up the good work")
+	} else {
+		fmt.Printf("NOt BAd NOT GOod")
+	}
+}
